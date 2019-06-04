e12c67c0b9e220c231fd72f3e3b143843d32a74c
HZH <Developer_HZH@hotmail.com>
2019-06-04 21:34:33 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,18 +1,33 @@
 package main
 
 import (
-	"bufio"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
+	"time"
 )
+
+var tgClient = &http.Client{Timeout: 10 * time.Second}
 
 func main() {
 	os.Setenv("HTTP_PROXY", "http://127.0.0.1:10086")
 	os.Setenv("HTTPS_PROXY", "http://127.0.0.1:10086")
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Please input telegram bot API token:")
-	API_TOKEN, _ := reader.ReadString('\n')
+	var API_TOKEN string
 
-	fmt.Printf("API Token:%s", API_TOKEN)
-	reader.ReadString('\n')
+	fmt.Println("Please input telegram bot API token:")
+	fmt.Scanf("%s", &API_TOKEN)
+	API_URL := "https://api.telegram.org/bot" + API_TOKEN + "/"
+	fmt.Printf("API URL:%s\n", API_URL)
+
+	resp, err := http.Get(API_URL + "getUpdates")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	var obj interface{}
+	json.Unmarshal([]byte(string(body)), &obj)
+	fmt.Println(obj.(map[string]interface{})["ok"])
 }
